a1500c7f380ef87e96e8f60dffd0c36a76b3e018
HANSIN <hansin.cho@emotion.co.kr>
2021-06-30 19:03:53 +0900

    xss 추가

--- src/main/java/com/example/how_to_use_model_mapper/controller/TestController.java
+++ src/main/java/com/example/how_to_use_model_mapper/controller/TestController.java
@@ -2,11 +2,14 @@ package com.example.how_to_use_model_mapper.controller;
 
 import com.example.how_to_use_model_mapper.dto.response.AttachResponse;
 import lombok.RequiredArgsConstructor;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.config.Configuration;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -19,7 +22,7 @@ public class TestController {
 	private final ModelMapper modelMapper;
 
 	@GetMapping(value = "/")
-	public String Test() {
+	public String test1() {
 
 		HashMap<String, Object> testMap = new HashMap<>();
 
@@ -61,4 +64,16 @@ public class TestController {
 		return "test";
 	}
 
+	@GetMapping(value = "/xss")
+	public String test() {
+		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		String param = "<a href='/xss' onclick='location.href=\"/test\";l'>TEST</a><script>window.alert('TEST');</script><img src='http://localhost:8081/a/b/c.png'>";
+		System.out.println("param = " + param);
+		String xss = Jsoup.clean(param, Whitelist.basic());  //원하는 값 기본적으로 xss 필터
+		String xss3 = Jsoup.clean(param, Whitelist.basicWithImages()); //원하는 값 중에 이미지를 빼고 xss
+		System.out.println("xss = " + xss);
+		System.out.println("xss3 = " + xss3);
+		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		return "test";
+	}
 }
